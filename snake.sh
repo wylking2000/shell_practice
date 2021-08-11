9b5899f8d30c0e47754bcdf323fc076080fdd828
yonglong.wyl <yonglong.wyl@alibaba-inc.com>
2021-08-11 10:50:54 +0800

    clean

--- snake.sh
+++ snake.sh
@@ -19,7 +19,7 @@ game_start=(
     '                                                 '
     '                ~~~ S N A K E ~~~                '
     '                                                 '
-    '                  Author:  LKJ                   '
+    '                                                 '
     '         space or enter   pause/play             '
     '         q                quit at any time       '
     '         s                change the speed       '
